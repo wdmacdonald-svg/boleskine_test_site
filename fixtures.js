2c556fafb2a11b309c17b5683eb1245f4346ab4c
wdmacdonald-svg <wdmacdonald-svg@users.noreply.github.com>
2026-07-26 12:37:31 +0100

    Update fixtures.js

--- fixtures.js
+++ fixtures.js
@@ -1,4 +1,11 @@
 var fixtureData = {
-    "has_fixture": false,
-    "last_updated": "2026-07-22T23:38:46+00:00"
+    "has_fixture": true,
+    "home_team": "Lochaber 2",
+    "away_team": "Boleskine",
+    "date_formatted": "Saturday, August 8th",
+    "time_formatted": "12:00 PM BST",
+    "location_formatted": "(A)",
+    "way": "away",
+    "status": "future",
+    "last_updated": "2026-07-26T11:34:54+00:00"
 };
