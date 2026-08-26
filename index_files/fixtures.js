8aec424d88337ab704babeb1fb5d7fd6a611f450
wdmacdonald-svg <wdmacdonald-svg@users.noreply.github.com>
2026-08-26 18:50:09 +0100

    Update fixtures logic and duplicate section

--- index_files/fixtures.js
+++ index_files/fixtures.js
@@ -7,5 +7,5 @@ var fixtureData = {
     "location_formatted": "Shawbost School, Shawbost, Western Isles (Away)",
     "way": "away",
     "status": "future",
-    "last_updated": "2026-08-26T07:43:30+00:00"
+    "last_updated": "2026-08-26T14:03:43+00:00"
 };
